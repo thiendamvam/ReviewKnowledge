e70d9c3dcec841a8535a706d24beb803c53bc757
Thien Dam <thienuit@gmail.com>
2013-11-01 19:24:40 +0700

    new code

---
+++ src/com/weatheronline/model/DateInfo.java
@@ -0,0 +1,52 @@
+package com.weatheronline.model;
+
+import java.io.Serializable;
+
+public class DateInfo implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String date;
+	private String status;
+	private String minTemp;
+	private String maxTemp;
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getMinTemp() {
+		return minTemp;
+	}
+
+	public void setMinTemp(String minTemp) {
+		this.minTemp = minTemp;
+	}
+
+	public String getMaxTemp() {
+		return maxTemp;
+	}
+
+	public void setMaxTemp(String maxTemp) {
+		this.maxTemp = maxTemp;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
--- src/com/weatheronline/view/WeatherDetailFragment.java
+++ src/com/weatheronline/view/WeatherDetailFragment.java
@@ -1,19 +1,75 @@
 package com.weatheronline.view;
 
-import com.weatheronline.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class WeatherDetailFragment extends Fragment{
+import com.weatheronline.R;
+import com.weatheronline.model.DateInfo;
+
+public class WeatherDetailFragment extends Fragment {
+	private final static String ARG_POSITION = "position";
+	private DateInfo mDateInfo;
+	private TextView mTvMaxTemp;
+	private TextView mTvMinTemp;
+	private TextView mTvStatus;
+	private ImageView mImgIcon;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		return inflater.inflate(R.layout.weather_detail_fragment, container, false);
+		View view = inflater.inflate(R.layout.weather_detail_fragment,
+				container, false);
+		/***
+		 * Init all field in the Screen
+		 */
+		initUI(view);
+
+		return view;
+	}
+
+	private void initUI(View view) {
+		// TODO Auto-generated method stub
+		mImgIcon = (ImageView) view.findViewById(R.id.imgIcon);
+		mTvStatus = (TextView) view.findViewById(R.id.tvStatus);
+		mTvMinTemp = (TextView) view.findViewById(R.id.tvFocasMinValue);
+		mTvMaxTemp = (TextView) view.findViewById(R.id.tvFocasMaxValue);
+	}
+
+	@Override
+	public void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		Bundle bundle = getArguments();
+		if (bundle != null) {
+			mDateInfo = (DateInfo) bundle.getSerializable("date_info");
+			if (mDateInfo != null)
+				exeDisplayDataToUI(mDateInfo);
+		}
+
+	}
+
+	private void exeDisplayDataToUI(DateInfo mDateInfo) {
+		try {
+			// TODO Auto-generated method stub
+			if (mDateInfo.getStatus() != null) {
+				mTvStatus.setText("" + mDateInfo.getStatus());
+			}
+			if (mDateInfo.getMinTemp() != null) {
+				mTvMinTemp.setText("" + mDateInfo.getMinTemp());
+			}
+			if (mDateInfo.getMaxTemp() != null) {
+				mTvMaxTemp.setText(mDateInfo.getMaxTemp());
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 	}
 }
